db989d87640267541015ab70e512b2ffd8435f59
RagulArumugam <ragula878@gmail.com>
2025-07-21 17:37:53 +0530

    initial commit

---
+++ tests/home.spec.ts
@@ -0,0 +1,6 @@
+import { test , expect } from '@playwright/test';
+
+// which is used to group the test
+test.describe( "description" ,() => {
+    test("Open HomePage and verify title", async ({ page }) => {})
+})
