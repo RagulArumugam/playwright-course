1137fc61f6d5ac1cec43322ce6f4c65afc20b1f6
RagulArumugam <ragula878@gmail.com>
2025-07-21 18:25:36 +0530

    basic tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,5 +2,11 @@ import { test , expect } from '@playwright/test';
 
 // which is used to group the test
 test.describe( "description" ,() => {
-    test("Open HomePage and verify title", async ({ page }) => {})
+    test("Open HomePage and verify title", async ({ page }) => {
+        // open url
+        await page.goto("https://practice.sdetunicorns.com/");
+
+        // verify title
+        await expect(page).toHaveTitle("Practice E-Commerce Site SDET Unicorns")
+    })
 })
